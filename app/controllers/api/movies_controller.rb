475e28c47fc420467699b9edfccf4d9a4b4cfe9c
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2021-01-23 14:06:48 -0600

    reverted to v42

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -308,7 +308,7 @@ class Api::MoviesController < ApplicationController
     while i < titles.length
   
       if titles[i] != nil && titles[i] != 0
-        p titles[i]
+        # p titles[i]
         request_overview_details("https://imdb8.p.rapidapi.com/title/get-overview-details?tconst=#{titles[i]}&currentCountry=US").delay
 
         parsed_results = JSON.parse(@results)
